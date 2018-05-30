50e1bff0fcf8ac88486c5bc709267686a6878f64
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-30 16:52:41 +0200

    fix opening hours & add online shop link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,29 +7,35 @@
     <section class="store-info">
         <div class="container clearfix">
             <ul class="list-inline m-0 float-left">
-                <li class="list-inline-item">
+                <li class="list-inline-item d-block d-sm-inline-block">
                     <i class="fas fa-home"></i>
                     <strong>Moin Hund</strong>
                     Ratsmühlendamm 26, 22335 Hamburg
                 </li>
-                <li class="list-inline-item">
+                <li class="list-inline-item d-block d-sm-inline-block">
                     <i class="fas fa-phone"></i>
                     +49 40 53889478
+                </li>
+                <li class="list-inline-item d-block d-sm-inline-block">
+                    @if($opening_hours[$now->dayOfWeek])
+                        @php($start = (clone $now)->hour(explode(':', $opening_hours[$now->dayOfWeek]['start'])[0])->minute(explode(':', $opening_hours[$now->dayOfWeek]['start'])[1])->second(0))
+                        @php($end = (clone $now)->hour(explode(':', $opening_hours[$now->dayOfWeek]['end'])[0])->minute(explode(':', $opening_hours[$now->dayOfWeek]['end'])[1])->second(0))
+                        <i class="fas @if($now->between($start, $end)) fa-door-open @else fa-door-closed @endif"></i>
+                        {{ $weekdays[$now->dayOfWeek] }}
+                        {{ $opening_hours[$now->dayOfWeek]['start'] }} - {{ $opening_hours[$now->dayOfWeek]['end'] }}
+                    @else
+                        <i class="fas fa-door-closed"></i>
+                        {{ $weekdays[$now->dayOfWeek] }}
+                        geschlossen
+                    @endif
                 </li>
             </ul>
 
             <span class="float-right">
-                @if($opening_hours[$now->dayOfWeek])
-                    @php($start = (clone $now)->hour(explode(':', $opening_hours[$now->dayOfWeek]['start'])[0])->minute(explode(':', $opening_hours[$now->dayOfWeek]['start'])[1])->second(0))
-                    @php($end = (clone $now)->hour(explode(':', $opening_hours[$now->dayOfWeek]['end'])[0])->minute(explode(':', $opening_hours[$now->dayOfWeek]['end'])[1])->second(0))
-                    <i class="fas @if($now->between($start, $end)) fa-door-open @else fa-door-closed @endif"></i>
-                    {{ $weekdays[$now->dayOfWeek] }}
-                    {{ $opening_hours[$now->dayOfWeek]['start'] }} - {{ $opening_hours[$now->dayOfWeek]['end'] }}
-                @else
-                    <i class="fas fa-door-closed"></i>
-                    {{ $weekdays[$now->dayOfWeek] }}
-                    geschlossen
-                @endif
+                <a href="https://moin-hund.de" style="color: #ffffff;">
+                    <i class="fas fa-shopping-basket"></i>
+                    Online-Shop
+                </a>
             </span>
         </div>
     </section>
