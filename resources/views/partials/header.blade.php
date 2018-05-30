2e867251d395087e9fa65b59e0f75b4cac61aa41
Tom Witkowski <dev.gummibeer@gmail.com>
2018-05-30 16:31:26 +0200

    fix opening hours

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,20 +8,28 @@
         <div class="container clearfix">
             <ul class="list-inline m-0 float-left">
                 <li class="list-inline-item">
-                    <i class="far fa-home"></i>
+                    <i class="fas fa-home"></i>
                     <strong>Moin Hund</strong>
                     Ratsmühlendamm 26, 22335 Hamburg
                 </li>
                 <li class="list-inline-item">
-                    <i class="far fa-phone"></i>
+                    <i class="fas fa-phone"></i>
                     +49 40 53889478
                 </li>
             </ul>
 
             <span class="float-right">
-                <i class="far fa-clock"></i>
-                {{ $weekdays[$now->dayOfWeek] }}
-                {{ $opening_hours[$now->dayOfWeek]['start'] }} - {{ $opening_hours[$now->dayOfWeek]['end'] }}
+                @if($opening_hours[$now->dayOfWeek])
+                    @php($start = (clone $now)->hour(explode(':', $opening_hours[$now->dayOfWeek]['start'])[0])->minute(explode(':', $opening_hours[$now->dayOfWeek]['start'])[1])->second(0))
+                    @php($end = (clone $now)->hour(explode(':', $opening_hours[$now->dayOfWeek]['end'])[0])->minute(explode(':', $opening_hours[$now->dayOfWeek]['end'])[1])->second(0))
+                    <i class="fas @if($now->between($start, $end)) fa-door-open @else fa-door-closed @endif"></i>
+                    {{ $weekdays[$now->dayOfWeek] }}
+                    {{ $opening_hours[$now->dayOfWeek]['start'] }} - {{ $opening_hours[$now->dayOfWeek]['end'] }}
+                @else
+                    <i class="fas fa-door-closed"></i>
+                    {{ $weekdays[$now->dayOfWeek] }}
+                    geschlossen
+                @endif
             </span>
         </div>
     </section>
