781dfdd80b948bee7c3aa01b8452f8d19bcfb24b
Tom Witkowski <dev.gummibeer@gmail.com>
2018-06-02 18:33:42 +0200

    update welcome text

--- resources/views/elements/welcome.blade.php
+++ resources/views/elements/welcome.blade.php
@@ -2,7 +2,7 @@
     <div class="container" style="height: 50vh;position: relative;">
         <img src="{!! $logo !!}" id="logo" />
         <div style="position: absolute;bottom: 7.5vh;right: 1rem;text-align: right;">
-            <span style="font-size:2rem;color: #ffffff;">Willkommen bei</span>
+            <span style="font-size:1.5rem;color: #ffffff;">Herzlich Willkommen bei</span>
             <h1 style="color: #ffffff;">Moin Hund</h1>
             <span style="font-size:1.5rem;color: #C0C0C0;">Feines für Fellnasen</span>
         </div>
